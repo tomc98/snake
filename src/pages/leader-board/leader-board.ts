beee83b01dba2e41c76c4a10d1107680be8b58e7
Thomas <tomcsere@gmail.com>
2018-05-30 09:15:53 +1000

    2: Auto stash before merge of "App-Dev-Assignment-2" and "origin/App-Dev-Assignment-2"

--- src/pages/leader-board/leader-board.ts
+++ src/pages/leader-board/leader-board.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 /**
  * Generated class for the LeaderBoardPage page.
@@ -15,62 +16,61 @@ import { NavController, NavParams } from 'ionic-angular';
 export class LeaderBoardPage {
 
   //dummy leader board list data
-  staticLoeaderBoardData = [
-    ["Jennifer",	"121"],
-    ["Amy",	"117"],
-    ["Joseph",	"117"],
-    ["Robert",	"117"],
-    ["Amba",	"111"],
-    ["Matthew",	"110"],
-    ["James",	"108"],
-    ["Jason",	"108"],
-    ["Justin",	"107"],
-    ["Stephanie",	"107"],
-    ["Lauren",	"105"],
-    ["Melissa",	"103"],
-    ["Daniel",	"101"],
-    ["Joshua",	"99"],
-    ["Sarah",	"99"],
-    ["David",	"97"],
-    ["Amanda",	"89"],
-    ["Nicole",	"83"],
-    ["Ashly",	"82"],
-    ["John",	"70"],
-    ["Andrew",	"32"],
-    ["Jessica",	"32"],
-    ["Micheal",	"32"]
-  ]
+  staticLoeaderBoardData = [];
+  sortedData = [];
+  low = 0;
+  table:HTMLTableElement = <HTMLTableElement>document.getElementById("leader_board_table");
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
+    storage.get("accounts").then((val) => {
+      this.staticLoeaderBoardData = val;
+      // console.log(this.staticLoeaderBoardData);
+    });
+    this.sortData();
+  }
+
+  sortData(){
+    this.low = 0;
+    while (this.staticLoeaderBoardData.length != this.sortedData.length){
+      for(var i = 0; i<=this.staticLoeaderBoardData.length; i++){
+        if (this.staticLoeaderBoardData[i].highscore == this.low){
+          this.sortedData.splice(0, 0, this.staticLoeaderBoardData[i]);
+        }
+      }
+      this.low += 1;
+      console.log("lol");
+    }
   }
 
   //insert leader board data into the page once html is loaded
   ionViewDidLoad() {
     console.log('ionViewDidLoad LeaderBoardPage');
-
+    this.sortData();
     this.populateTable();
   }
-  
+
   //on back button pressed, pop to root
   onClickBack(){
-    this.navCtrl.popToRoot()
+    this.navCtrl.popToRoot();
   }
 
   //adds the leader board data to the html table
   populateTable(){
-    var table:HTMLTableElement = <HTMLTableElement>document.getElementById("leader_board_table");
-
+  console.log("what?");
+    this.low = 0;
     //for data entry
-    this.staticLoeaderBoardData.forEach(element => {
-      
-      var row = table.insertRow();
+    while(this.low < this.sortedData.length){
+
+      var row = this.table.insertRow();
 
       var name = row.insertCell();
-      name.innerHTML = <string>element[0]; 
+      name.innerHTML = <string>this.sortedData[this.low].username;
 
       var score = row.insertCell();
-      score.innerHTML = <string>element[1]; 
-
-    });
+      score.innerHTML = <string>this.sortedData[this.low].highscore;
+      console.log("in");
+      console.log("between");
+      this.low += 1;
+    }
   }
 }
